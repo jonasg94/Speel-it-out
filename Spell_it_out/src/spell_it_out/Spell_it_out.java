e96a5894ff95e7f262c17a1fa46abecd7332abdf
Joao Gomes <jonasg94@users.noreply.github.com>
2017-09-20 16:24:19 +0100

    Speel
    criação do projeot NetBeans

---
+++ Spell_it_out/src/spell_it_out/Spell_it_out.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package spell_it_out;
+
+/**
+ *
+ * @author joaogomes
+ */
+public class Spell_it_out {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
